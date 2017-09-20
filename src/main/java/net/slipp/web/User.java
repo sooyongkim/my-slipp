612816b69920edbc6185774d3257aeb04719e1df
sooyongkim <sooyong82@gmail.com>
2017-09-20 17:56:08 +0900

    create login and user list pages.

---
+++ src/main/java/net/slipp/web/User.java
@@ -0,0 +1,30 @@
+package net.slipp.web;
+
+public class User {
+
+	private String userId;
+	private String name;
+	private String password;
+	private String email;
+	
+	
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", name=" + name + ", password=" + password + ", email=" + email + "]";
+	}
+	
+	
+}
--- src/main/java/net/slipp/web/WelcomeController.java
+++ src/main/java/net/slipp/web/WelcomeController.java
@@ -1,5 +1,18 @@
 package net.slipp.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class WelcomeController {
 
+	@GetMapping("/helloworld")
+	public String welcome(String name, @RequestParam(value = "i", required = false, defaultValue = "33") Integer age, Model model){
+		model.addAttribute("name", name);
+		model.addAttribute("age", age);
+		System.out.println("name : " + name);
+		return "welcome";
+	}
 }
